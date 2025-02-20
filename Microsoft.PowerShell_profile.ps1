1871fb523517ca99720e77b5fe0579d1d7b8ab87
ArtiAasav <arti@hkhk.edu.ee>
2025-02-20 03:35:15 -0800

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+$Host.UI.RawUI.WindowTitle = "Arti"
+Clear-Host
+Write-Host "`t********metshein.com********"
+Write-Host "`t `t Õpime PowerShelli :)"
+Write-host " "
+set-location c:\Users\Administrator\Documents\WindowsPowerShell
